aba9b5212cf329ac1355e3b54a0755cfda67c384
aamir-khan <aamir@edx.org>
2015-10-16 11:49:05 +0500

    ECOM-2430: changed track to trackLink and also removed the upgrade button tracking because this is already being tracked in legacy.js

--- lms/static/js/dashboard/track_events.js
+++ lms/static/js/dashboard/track_events.js
@@ -12,15 +12,14 @@ var edx = edx || {};
             course_image_link = $(".dashboard-course-image"),
             enter_course_link = $(".enter-course-link"),
             course_action_more = $(".wrapper-action-more"),
-            course_action_upgrade = $(".action-upgrade"),
             find_courses_btn = $(".btn-find-courses"),
             course_learn_verified = $(".learned-verified-track");
 
         // Fire analytics events when the "course title link" button is clicked.
         course_title_link.on("click", function (event) {
             var courseKey = $(event.target).data("course-key");
-            analytics.track(
-                "edx.bi.dashboard.clicked_course_title_link",
+            analytics.trackLink(
+                "edx.bi.dashboard.course_title.clicked",
                 {
                     category: "dashboard",
                     label: courseKey
@@ -30,9 +29,9 @@ var edx = edx || {};
 
         // Fire analytics events when the "course image" is clicked.
         course_image_link.on("click", function (event) {
-            var courseKey = $(event.target).parent().data("course-key");
-            analytics.track(
-                "edx.bi.dashboard.clicked_course_image",
+            var courseKey = $(event.target).closest(".dashboard-course-image").data("course-key");
+            analytics.trackLink(
+                "edx.bi.dashboard.course_image.clicked",
                 {
                     category: "dashboard",
                     label: courseKey
@@ -43,8 +42,8 @@ var edx = edx || {};
         // Fire analytics events when the "View Course" button is clicked.
         enter_course_link.on("click", function (event) {
             var courseKey = $(event.target).data("course-key");
-            analytics.track(
-                "edx.bi.dashboard.clicked_enter_course_link",
+            analytics.trackLink(
+                "edx.bi.dashboard.enter_course.clicked",
                 {
                     category: "dashboard",
                     label: courseKey
@@ -54,26 +53,9 @@ var edx = edx || {};
 
         // Fire analytics events when the "Settings" button is clicked.
         course_action_more.on("click", function (event) {
-            var courseKey = $(event.target).parent().data("course-key");
-            // If the user clicked on the fa-cog icon then change the dom hierarchy.
-            if(courseKey === undefined){
-                courseKey = $(event.target).parent().parent().data("course-key");
-            }
-            event.stopPropagation();
-            analytics.track(
-                "edx.bi.dashboard.clicked_more_action",
-                {
-                    category: "dashboard",
-                    label: courseKey
-                }
-            );
-        });
-
-        // Fire analytics events when the "Upgrade" button is clicked.
-        course_action_upgrade.on("click", function (event) {
-            var courseKey = $(event.target).parent().parent().data("course-id");
-            analytics.track(
-                "edx.bi.dashboard.clicked_course_upgrade_link",
+            var courseKey = $(event.target).closest(".wrapper-action-more").data("course-key");
+            analytics.trackLink(
+                "edx.bi.dashboard.more_action_button.clicked",
                 {
                     category: "dashboard",
                     label: courseKey
@@ -84,8 +66,8 @@ var edx = edx || {};
         // Fire analytics events when the "Learned about verified track" link is clicked.
         course_learn_verified.on("click", function (event) {
             var courseKey = $(event.target).data("course-key");
-            analytics.track(
-                "edx.bi.dashboard.clicked_learned_verified_link",
+            analytics.trackLink(
+                "edx.bi.dashboard.learn_verified.clicked",
                 {
                     category: "dashboard",
                     label: courseKey
@@ -95,11 +77,11 @@ var edx = edx || {};
 
         // Fire analytics events when the "Find Courses" button is clicked.
         find_courses_btn.on("click", function () {
-            analytics.track(
-                "edx.bi.dashboard.clicked_enter_course_link",
+            analytics.trackLink(
+                "edx.bi.dashboard.find_course_button.clicked",
                 {
                     category: "dashboard",
-                    label: "edx_find_new_courses_button"
+                    label: "find_course_button"
                 }
             );
         });
